de41a9ac6403a4358fbae8789a3e739cc6e2dca8
Heba Ali <hebanassif19@gmail.com>
2022-01-09 23:08:30 +0200

    Update Landmark Detection

--- Landmark Detection/FaceAligner.py
+++ Landmark Detection/FaceAligner.py
@@ -21,37 +21,8 @@ class FaceAligner:
         return result
 
     def align(self, image, leftEyeCenter, rightEyeCenter):
-        # compute the angle between the eye centroids
         dY = rightEyeCenter[1] - leftEyeCenter[1]
         dX = rightEyeCenter[0] - leftEyeCenter[0]
         angle = np.degrees(np.arctan2(dY, dX)) - 180
         result = self.rotate_image(image, angle)
-        # compute the desired right eye x-coordinate based on the
-        # desired x-coordinate of the left eye
-        desiredRightEyeX = 1.0 - self.desiredLeftEye[0]
-        # determine the scale of the new resulting image by taking
-        # the ratio of the distance between eyes in the *current*
-        # image to the ratio of distance between eyes in the
-        # *desired* image
-        dist = np.sqrt((dX ** 2) + (dY ** 2))
-        desiredDist = (desiredRightEyeX - self.desiredLeftEye[0])
-        desiredDist *= self.desiredFaceWidth
-        scale = desiredDist / dist
-        
-        # compute center (x, y)-coordinates (i.e., the median point)
-        # between the two eyes in the input image
-        eyesCenter = ((leftEyeCenter[0] + rightEyeCenter[0]) // 2,(leftEyeCenter[1] + rightEyeCenter[1]) // 2)
-        # grab the rotation matrix for rotating and scaling the face
-        M = cv2.getRotationMatrix2D(eyesCenter, angle, scale)
-        # update the translation component of the matrix
-        tX = self.desiredFaceWidth * 0.5
-        tY = self.desiredFaceHeight * self.desiredLeftEye[1]
-        M[0, 2] += (tX - eyesCenter[0])
-        M[1, 2] += (tY - eyesCenter[1])
-        
-        # apply the affine transformation
-        (w, h) = (self.desiredFaceWidth, self.desiredFaceHeight)
-        output = cv2.warpAffine(image, M, (w, h),flags=cv2.INTER_CUBIC)
-        
-        # return the aligned face
         return result
